335d56333189a08c3862d2c5e99811487d8cc62b
Daniil Dron <dron@element-soft.com>
2025-05-19 11:36:14 +0300

    filter

--- src/CountrySelector.tsx
+++ src/CountrySelector.tsx
@@ -1,37 +1,42 @@
-import { use, useEffect } from "react";
+import { use, useEffect, type ChangeEvent } from "react";
 import { CountryContext, type Country } from "./CountryContext";
 
 const COUNTRY_ID_DEFAULT = 1;
 
+const findCountryById = (countries: Array<Country | null> | null, id: number) =>
+  countries?.find(country => country?.id === id) ?? null;
+
 export default function CountrySelector() {
   const {countries,selectedCountry,setSelectedCountry} = use(CountryContext);
 
-  // TODO:change later
   useEffect(() => {
     if (!selectedCountry && countries?.length) {
-      const defaultCountry = countries.find(c => c?.id === COUNTRY_ID_DEFAULT);
+      const defaultCountry = findCountryById(countries, COUNTRY_ID_DEFAULT);
       if (defaultCountry) {
-        setSelectedCountry(defaultCountry);
-      }
+        setSelectedCountry(defaultCountry)
+      };
     }
   }, [selectedCountry, countries, setSelectedCountry]);
 
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selected = findCountryById(countries, Number(e.target.value));
+    if (selected) setSelectedCountry(selected);
+  };
+
   return (
+    <div className="flex gap-2 items-center">
+    <label>Выбрать страну</label>
     <select
       className="p-2 border rounded"
       value={selectedCountry?.id}
-      onChange={(e) => {
-        const selected = countries?.find((c: Country | null) => c?.id === Number(e.target.value));
-        if (selected) setSelectedCountry({ id: selected.id, name: selected.name });
-      }}
-      
-    >
-      <option value="">Select country</option>
+      onChange={handleChange}
+      >
       {countries?.map((c: Country | null) => (
         <option key={c?.id} value={c?.id}>
           {c?.name}
         </option>
       ))}
     </select>
+    </div>
   );
 }
